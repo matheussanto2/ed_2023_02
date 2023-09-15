747176104b0ebd7679c034a317a2d0dcc542e7eb
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-09-15 20:56:46 -0300

    P_2_4_Msf.ts

--- P_2_4_Msf.ts
+++ P_2_4_Msf.ts
@@ -1,20 +1,46 @@
-function fibonacci(n: number, depth: number = 0): number {
-    let tabs = "";
-    for (let i = 0; i < depth; i++) {
-        tabs += "  "; // 
-    }
-    console.log(`${tabs}Calculando Fibonacci(${n})`);
+class CallNode {
+    constructor(public n: number, public depth: number) {}
+}
 
-    if (n <= 0) {
-        return 0;
-    } else if (n === 1) {
-        return 1;
-    } else {
-        const result = fibonacci(n - 1, depth + 1) + fibonacci(n - 2, depth + 1);
-        return result;
+class FibonacciTree {
+    root: CallNode;
+    children: FibonacciTree[] = [];
+
+    constructor(n: number, depth: number) {
+        this.root = new CallNode(n, depth);
+    }
+
+    addChild(child: FibonacciTree) {
+        this.children.push(child);
+    }
+
+    printTree() {
+        const tabs = '  '.repeat(this.root.depth);
+        console.log(`${tabs}Calculando Fibonacci(${this.root.n})`);
+
+        for (const child of this.children) {
+            child.printTree();
+        }
+    }
+
+    calculate(): number {
+        if (this.root.n <= 0) {
+            return 0;
+        } else if (this.root.n === 1) {
+            return 1;
+        } else {
+            const left = new FibonacciTree(this.root.n - 1, this.root.depth + 1);
+            const right = new FibonacciTree(this.root.n - 2, this.root.depth + 1);
+            this.addChild(left);
+            this.addChild(right);
+            return left.calculate() + right.calculate();
+        }
     }
 }
 
-const n = 5;  // O número de Fibonacci que você deseja calcular
-const fibonacci_n = fibonacci(n);
-console.log(`O ${n}-ésimo termo da sequência de Fibonacci é: ${fibonacci_n}`);
+const n = 5; // O número de Fibonacci que você deseja calcular
+const fibonacciTree = new FibonacciTree(n, 0);
+console.log(`Árvore de Chamadas para Fibonacci(${n}):\n`);
+fibonacciTree.calculate();
+console.log(`\nO ${n}-ésimo termo da sequência de Fibonacci é: ${fibonacciTree.calculate()}`);
+
