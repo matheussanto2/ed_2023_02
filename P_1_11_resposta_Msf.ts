1178de129770b029f51e89993a58f34c920c5281
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-09-22 20:13:10 -0300

    Create P_1_11_resposta_Msf.ts

---
+++ P_1_11_resposta_Msf.ts
@@ -0,0 +1,3 @@
+A dificuldade ao lidar com três condições (maior, menor ou igual) usando o bloco switch é que ele não é ideal para comparações numéricas, especialmente quando se trabalha com valores contínuos como n1 e n2. 
+  O if-else é uma escolha mais clara e versátil para essas situações. 
+  O switch funciona melhor quando se verifica valores específicos, como estados ou opções predefinidas, enquanto o if-else é mais flexível e fácil de entender para comparações numéricas.
