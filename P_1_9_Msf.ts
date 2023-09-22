522ab0939b2c06b994a4d8088a0de1e9e20a4b8f
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-09-22 20:05:12 -0300

    Create P_1_9_Msf.ts

---
+++ P_1_9_Msf.ts
@@ -0,0 +1,6 @@
+let numero = 99; // Iniciamos com o maior múltiplo de 3 menor ou igual a 100 (99).
+
+while (numero >= 0) {
+  console.log(numero);
+  numero -= 3; // Subtrai 3 para obter o próximo múltiplo de 3 em ordem decrescente.
+}
