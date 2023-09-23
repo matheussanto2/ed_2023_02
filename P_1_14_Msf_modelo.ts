05925eeacdfb9a17791765d31a9a6c371c08be24
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-09-22 21:32:00 -0300

    Create P_1_14_Msf_modelo.ts

---
+++ P_1_14_Msf_modelo.ts
@@ -0,0 +1,5 @@
+export let nome: string = "Alice";
+export let idade: number = 30;
+export let arrayCadeia: string[] = ["Maçã", "Banana", "Laranja"];
+export let arrayNumeros: number[] = [1, 2, 3, 4, 5];
+export let pessoa: { nome: string, idade: number } = { nome: "Bob", idade: 25 };
