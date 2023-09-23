3ffa994122618b3e4f5322994bf9d3cb7b28bc56
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-09-22 21:59:20 -0300

    Create TC_2_1_MSFS.ts

---
+++ TC_2_1_MSFS.ts
@@ -0,0 +1,8 @@
+É importante, para qualquer profissional da área de computação que trabalhe com desenvolvimento, que os conceitos de recursão e iteração sejam bem diferenciados. 
+  Um problema recursivo corresponde a um problema geral e demanda uma abordagem recursiva. Já uma abordagem iterativa corresponde a um desdobramento do problema.
+Um problema recursivo é resolvido desmembrando-se o problema em um problema menor até que se chegue à menor instância possível, chamada de caso base. 
+  A solução para o problema é então obtida combinando-se sucessivamente o resultado a instâncias menores até que se chegue à instância original.
+A complexidade espacial diz respeito à quantidade de memória utilizada por um programa. 
+  A complexidade de tempo diz respeito às operações envolvidas no programa, 
+  já a complexidade de chamada de função diz respeito aos dados temporários que são necessários para que o programa inicie e termine sua execução. 
+  Alterar a maneira como um algoritmo é codificado para um programa pode aumentar ou diminuir sua complexidade temporal e sua complexidade espacial.
