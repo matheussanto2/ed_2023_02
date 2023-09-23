4e2d3036401d9567b40dbaba577e66766ff4c378
matheussanto2 <126420034+matheussanto2@users.noreply.github.com>
2023-09-22 21:34:04 -0300

    Create TC.1.1._Msf.ts

---
+++ TC.1.1._Msf.ts
@@ -0,0 +1,5 @@
+Um algoritmo corresponde a um conjunto de passos (1) a serem efetuados para a resolução de um problema. Os passos (2) ou instruções (3) definidos em um algoritmo devem ser sequenciais, ou seja, o algoritmo deve possuir início e fim bem definidos. Algoritmos podem ser vistos também como funções (5) funcionais que transformam um conjunto de dados de entrada (6) em um conjunto de resultados de saída (7).
+
+Os blocos fundamentais que podem constituir um algoritmo incluem tipicamente atribuições (8), estruturas condicionais (9), estruturas de repetição (10), operações aritméticas (11), operações lógicas (12) e chamadas de funções (13).
+
+Sob o ponto de vista de entrada e saída, pode-se dizer que algoritmos determinísticos (14) são aqueles que obtêm uma mesma correspondência input-output (15) considerando as mesmas entradas (16).
